0195f0e56bfd047b2eb4aa31f1c9501bffcf451f
Maric Brandi <theoneandonlymandarr@gmail.com>
2020-11-04 23:19:17 -0500

    add reconcileSell, reconcileBuy, reconcileOrder

--- orderBook.js
+++ orderBook.js
@@ -1,40 +1,92 @@
-// const updatedBook = reconcileOrder(existingBook, incomingOrder)
-function reconcileSell(existingBook, incomingOrder) {
-  const foundIndex = existingBook.findIndex(existingItem => existingItem.type === 'buy'
-    && incomingOrder.price <= existingItem.price)
-  console.log(foundIndex)
-  if (foundIndex >= 0) {
-    if (existingBook[foundIndex].quantity > incomingOrder.quantity) {
-      const reducedOrder = existingBook[foundIndex]
-      reducedOrder.quantity -= incomingOrder.quantity
-      existingBook.splice(foundIndex, 1)
-      existingBook.push(reducedOrder)
-    } else if (existingBook[foundIndex].quanity < incomingOrder.quantity) {
-      incomingOrder.quanity -= existingBook[foundIndex]
-      existingBook.push(incomingOrder)
-      existingBook.splice(foundIndex, 1)
-    } else {
-      existingBook.splice(foundIndex, 1)
-    }
+// Sell incoming --> if incoming contains sell for <= price than a buy in existingBook,
+//    execute order
+//  else
+//    add to existingBook
+// Buy incoming --> if incoming contains buy for >= price than a sell in existingBook, 
+//    execute order
+//  else
+//    add to existingBook 
 
-  } else {
+// Incoming sell order function
+
+function reconcileSell(existingBook, incomingOrder) {
+  let foundIndex = 0
+  let reducedIncomingOrder = 0
+  do {
+    foundIndex = existingBook.findIndex(existingItem => existingItem.type === 'buy'
+      && incomingOrder.price <= existingItem.price)
+
+    if (foundIndex >= 0) {
+      if (existingBook[foundIndex].quantity >= incomingOrder.quantity) {
+        const reducedExistingOrder = existingBook[foundIndex]
+        const { quantity } = existingBook[foundIndex]
+        reducedExistingOrder.quantity -= incomingOrder.quantity
+        // ln 26 stores qty of incoming order after its value is used to reduce a matching order's qty in 
+        // existingBook, for while loop condition incomingOrder.quantity
+        incomingOrder.quantity = incomingOrder.quantity - quantity > 0 ? incomingOrder.quantity - quantity : 0
+        existingBook.splice(foundIndex, 1)
+        if (reducedExistingOrder.quantity > 0) {
+          existingBook.push(reducedExistingOrder)
+        }
+      } else if (existingBook[foundIndex].quantity < incomingOrder.quantity) {
+        reducedIncomingOrder = incomingOrder
+        reducedIncomingOrder.quantity -= existingBook[foundIndex].quantity
+        existingBook.splice(foundIndex, 1)
+
+      } else {
+        existingBook.splice(foundIndex, 1)
+      }
+    }
+  } while (foundIndex >= 0 && incomingOrder.quantity)
+
+
+  if (incomingOrder.quantity) {
     existingBook.push(incomingOrder)
   }
+
   return existingBook
 }
+
+// Incoming buy order function
 
 function reconcileBuy(existingBook, incomingOrder) {
-  const foundIndex = existingBook.findIndex(existingItem => existingItem.type === 'sell'
-    && incomingOrder.quantity <= existingItem.quantity
-    && incomingOrder.price >= existingItem.price)
-  console.log(foundIndex)
-  if (foundIndex >= 0) {
-    existingBook.splice(foundIndex, 1)
-  } else {
+  let foundIndex = 0
+  let reducedIncomingOrder = 0
+  do {
+    foundIndex = existingBook.findIndex(existingItem => existingItem.type === 'sell'
+      && incomingOrder.price >= existingItem.price)
+
+    if (foundIndex >= 0) {
+      if (existingBook[foundIndex].quantity >= incomingOrder.quantity) {
+        const reducedExistingOrder = existingBook[foundIndex]
+        const { quantity } = existingBook[foundIndex]
+        reducedExistingOrder.quantity -= incomingOrder.quantity
+        // see ln 24 comment 
+        incomingOrder.quantity = incomingOrder.quantity - quantity > 0 ? incomingOrder.quantity - quantity : 0
+        existingBook.splice(foundIndex, 1)
+        if (reducedExistingOrder.quantity > 0) {
+          existingBook.push(reducedExistingOrder)
+        }
+      } else if (existingBook[foundIndex].quantity < incomingOrder.quantity) {
+        reducedIncomingOrder = incomingOrder
+        reducedIncomingOrder.quantity -= existingBook[foundIndex].quantity
+        existingBook.splice(foundIndex, 1)
+
+      } else {
+        existingBook.splice(foundIndex, 1)
+      }
+    }
+  } while (foundIndex >= 0 && incomingOrder.quantity)
+
+
+  if (incomingOrder.quantity) {
     existingBook.push(incomingOrder)
   }
+
   return existingBook
 }
+
+// Switch bt/w reconciling sell and buy orders
 
 function reconcileOrder(existingBook, incomingOrder) {
 
@@ -46,24 +98,3 @@ function reconcileOrder(existingBook, incomingOrder) {
   }
 }
 module.exports = reconcileOrder
-
-/*
- updatedBook.push(incomingOrder)
-        // existingBook is an array [] and incomingOrder is an object {}
-        // incomingOrder.type works for objects to return the type property ('sell')
-        // for existingBook, call 1st element of an array existingBook[0], and then use .type
-      } else if (existingBook[0].type == incomingOrder.type) {
-        updatedBook.push(incomingOrder)
-        // index set to zero for all properties b/c existingBook contains one object in an array
-      } else if ((existingBook[0].quantity != incomingOrder.quantity) || (existingBook[0].price != incomingOrder.price)) {
-        updatedBook.push(incomingOrder)
-        // how do I check incomingOrder against ALL elements of existingBook??
-      } else if (existingBook[0].type != incomingOrder.type && existingBook[0].quantity == incomingOrder.quantity && existingBook[0].price == incomingOrder.price) {
-        // fulfills an order and removes the matching order
-        for (let i = 0; i < existingBook.length; i++) {
-          if (existingBook[i].type != incomingOrder.type && existingBook[i].quantity == incomingOrder.quantity && existingBook[i].price == incomingOrder.price) {
-            existingBook.splice(i, (i + 1))
-          }
-
-        }
-        */
